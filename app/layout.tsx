11064c27426f099e1185f6775657282163a6fe9a
austinmarklord-web <austinmarklord@gmail.com>
2026-09-19 20:45:30 +0100

    Load centered mobile mandate styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { DM_Serif_Display, Inter } from "next/font/google";
 import "./site.css";
 import "./hero-mobile.css";
+import "./mandate-mobile-center.css";
 import { siteUrl } from "../lib/site-config";
 import { ScrollToTop } from "../components/scroll-to-top";
 
